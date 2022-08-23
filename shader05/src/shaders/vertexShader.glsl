298816bf5ade9f777517fb6be2ca9e126b1427ef
watanabe_masahiro02 <watanabe_masahiro@5star.co.jp>
2022-08-23 15:16:50 +0900

    shader

---
+++ shader05/src/shaders/vertexShader.glsl
@@ -0,0 +1,13 @@
+uniform vec2 uFrequency;
+uniform float uTime;
+
+void main(){
+  vec4 modelPosition = modelMatrix * vec4(position,1.0);
+
+  modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.05;
+  modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.05;
+
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectionPosition = projectionMatrix * viewPosition;
+  gl_Position = projectionPosition;
+}
