5fb66d277068e86c95f6d194b5be4ec9b1ebb7fb
watanabe_masahiro02 <watanabe_masahiro@5star.co.jp>
2022-08-25 10:36:13 +0900

    07-cameras

--- journey/07-cameras/src/script.js
+++ journey/07-cameras/src/script.js
@@ -1,17 +1,30 @@
 import './style.css'
 import * as THREE from 'three'
-
-/**
- * Base
- */
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Sizes
 const sizes = {
     width: 800,
     height: 600
 }
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+})
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
@@ -43,11 +56,19 @@ const camera = new THREE.PerspectiveCamera(
 //     100 //far
 // )
 
-camera.position.x = 2
-camera.position.y = 2
+// camera.position.x = 2
+// camera.position.y = 2
 camera.position.z = 2
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera,canvas);
+controls.enableDamping = true;
+// controls.target.y = 2;
+// controls.update();//controlsを更新したあとはupdateメソッドを必ず呼び出す
+//Damping アニメーションを滑らかにする
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -63,7 +84,15 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+
+    // Update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 3
+    // camera.lookAt(mesh.position)
+
+    controls.update();//Dampingを使う時は必ずフレームごとにアップデートする
 
     // Render
     renderer.render(scene, camera)
